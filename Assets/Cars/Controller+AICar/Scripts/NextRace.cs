d24b71109ff0d70a38cc5aaf135b8a2542cfe112
DEV-ajthedj <theajayisbetter@gmail.com>
2026-01-30 21:47:13 -0500

    Added Loaders to Asynchronously Load the Next Scenes (Races)

--- Assets/Cars/Controller+AICar/Scripts/NextRace.cs
+++ Assets/Cars/Controller+AICar/Scripts/NextRace.cs
@@ -1,22 +1,43 @@
-using Unity.VectorGraphics;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.CrashReportHandler;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NextRace : MonoBehaviour
 {
+    public int nextSceneIndex;
+    private PrometeoWaypointAI carAI;
+    public TMPro.TextMeshPro progressText;
 
-    private PrometeoWaypointAI waypointAI;
+    private AsyncOperation load;
 
+    void Awake()
+    {
+        CrashReportHandler.enableCaptureExceptions = false;
+    }
     void Start()
     {
-        waypointAI = GetComponent<PrometeoWaypointAI>();
+        carAI = GetComponent<PrometeoWaypointAI>();
+        load = SceneManager.LoadSceneAsync(nextSceneIndex);
+        load.allowSceneActivation = false;
     }
 
     void Update()
     {
-        if (waypointAI.IsComplete())
+        if (progressText != null)
+            progressText.text = "Loading: " + (load.progress * 100).ToString() + "%";
+        if (carAI == null)
         {
-            SceneManager.LoadScene(1);
+            Debug.Log("Blah");
+            if (load.progress >= 0.89999)
+                load.allowSceneActivation = true;
+        }
+        else
+        {
+            if (carAI.IsComplete() && load.progress >= 0.89999)
+                load.allowSceneActivation = true;
         }
     }
+
 }
